f777514f648746e6dd6876e5d2b7818051c1fff6
Danut Codrescu <danut.codrescu@collibra.com>
2025-02-12 23:00:54 +0100

    style: translate pdf to swedish

--- app/components/PDF.server.tsx
+++ app/components/PDF.server.tsx
@@ -1,4 +1,5 @@
 import { Document, Link, Page, Text, View } from '@react-pdf/renderer';
+
 import { CompensationDataPerCompanyPerYear } from '~/utils/server/repositories/compensation.server';
 
 export function PDF(data: CompensationDataPerCompanyPerYear) {
@@ -20,58 +21,59 @@ export function PDF(data: CompensationDataPerCompanyPerYear) {
         }}
       >
         <View style={{ marginBottom: 40 }}>
-          <Text style={{ fontSize: 20, marginBottom: 12 }}>Specified compensation for cigarette butt collection</Text>
+          <Text style={{ fontSize: 20, marginBottom: 12 }}>Specificerad ersättning för insamling av fimpar</Text>
           <Text style={{ fontSize: 20, marginBottom: 24 }}>
-            {data.companyName} year {data.year}
+            {data.companyName} år {data.year}
           </Text>
         </View>
 
         <View style={{ marginBottom: 32 }}>
           <Text style={{ marginBottom: 24, textAlign: 'justify' }}>
-            According to chapter 8 of the agreement with SUP Filter, your municipality is entitled to annual
-            compensation for the collection of cigarette butts. Appendix 8.1 of the agreement states the compensation
-            amounts that apply, how they are adjusted each year and what they are based on. Information on payment terms
-            and invoicing can be found in Appendix 8.2.
+            Enligt avtal med SUP Filter är er kommun berättigad till årlig ersättning för insamling av fimpar. I
+            avtalets kapitel 8 och bilaga 8.1 framgår vilka ersättningsbelopp som gäller, hur de justeras varje år och
+            vilka antaganden de grundas på. Information om betalningsvillkor och fakturering återfinns i bilaga 8.2.
           </Text>
         </View>
 
         <View style={{ marginBottom: 32 }}>
           <Text style={{ marginBottom: 12 }}>
-            Total compensation:{' '}
+            Total ersättning:&nbsp;
             <Text style={{ fontFamily: 'Helvetica-Bold' }}>
-              {Intl.NumberFormat('sv-SE').format(data?.total as number)} SEK
-            </Text>{' '}
-            (A + B + C) x (D) x (E)
+              {Intl.NumberFormat('sv-SE').format(data?.total as number)} kronor
+            </Text>
+            &nbsp; (A + B + C) x (D) {data?.typeOfAgreement === 'old' ? ' x (E)' : ''}
           </Text>
         </View>
 
         <View>
           <Text style={{ marginBottom: 8 }}>
-            A) Variable remuneration:{' '}
+            A) Rörlig ersättning:&nbsp;
             <Text style={{ fontFamily: 'Helvetica-Bold' }}>
-              {Intl.NumberFormat('sv-SE').format(data?.variableCompensation as number)} SEK
-            </Text>{' '}
-            (&nbsp;{Intl.NumberFormat('sv-SE', { minimumFractionDigits: 15 }).format(0.735147372938808)} SEK x &nbsp;
-            {Intl.NumberFormat('sv-SE').format(data?.inhabitants as number)}&nbsp;inhabitants&nbsp;)
+              {Intl.NumberFormat('sv-SE').format(data?.variableCompensation as number)} kronor
+            </Text>
+            (&nbsp;{Intl.NumberFormat('sv-SE', { minimumFractionDigits: 15 }).format(data.yearSekAdmin)} SEK x &nbsp;
+            {Intl.NumberFormat('sv-SE').format(data.inhabitants as number)}&nbsp;inhabitants&nbsp;)
           </Text>
           <Text style={{ marginBottom: 8 }}>
-            B) Reporting remuneration:{' '}
-            <Text style={{ fontFamily: 'Helvetica-Bold' }}>{Intl.NumberFormat('sv-SE').format(8904)} SEK</Text>
-          </Text>
-          <Text style={{ marginBottom: 8 }}>
-            C) Surcharge on the reporting fee:{' '}
+            B) Rapporteringsersättning:&nbsp;
             <Text style={{ fontFamily: 'Helvetica-Bold' }}>
-              {Intl.NumberFormat('sv-SE', { minimumFractionDigits: 1 }).format(data?.surcharge as number)} SEK
+              {Intl.NumberFormat('sv-SE').format(data.adminFee)} kronor
             </Text>
           </Text>
           <Text style={{ marginBottom: 8 }}>
-            D) Change factor with KPI:{' '}
+            C) Påslag på rapporteringsersättningen:&nbsp;
+            <Text style={{ fontFamily: 'Helvetica-Bold' }}>
+              {Intl.NumberFormat('sv-SE', { minimumFractionDigits: 1 }).format(data.surcharge as number)} kronor
+            </Text>
+          </Text>
+          <Text style={{ marginBottom: 8 }}>
+            D) Förändringsfaktor med KPI::&nbsp;
             <Text style={{ fontFamily: 'Helvetica-Bold' }}>
               {Intl.NumberFormat('sv-SE', { minimumFractionDigits: 6 }).format(data?.changeFactor as number)}
             </Text>
           </Text>
           <Text style={{ marginBottom: 8 }}>
-            E) Change factor littering rate:{' '}
+            E) Change factor littering rate:&nbsp;
             <Text style={{ fontFamily: 'Helvetica-Bold' }}>
               {Intl.NumberFormat('sv-SE', { minimumFractionDigits: 6 }).format(data?.changeFactorLitter as number)}
             </Text>
@@ -80,7 +82,7 @@ export function PDF(data: CompensationDataPerCompanyPerYear) {
 
         <View style={{ marginTop: 'auto', fontSize: 8 }}>
           <Text>
-            If you have any questions, please contact SUP Filter via{' '}
+            Vid eventuella frågor vänligen kontakta SUP Filter via&nbsp;
             <Link href="mailto:info@supfilter.se">info@supfilter.se</Link>.
           </Text>
         </View>
